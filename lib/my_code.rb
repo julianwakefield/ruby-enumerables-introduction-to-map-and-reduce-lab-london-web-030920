e686e09affd4d9762d5832a5449fa6b0dea27827
Julian Wakefield <julianwakefield@outlook.com>
2020-03-08 19:25:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # My Code here....
+def map_to_negativize(source, array)
+
+map([1,2,3, -9]) => ([-1,-2,-3,9])
+end
+
+def map_to_no_change(source, array)
+
+end
+
+def map_to_double(source, array)
+
+end
+
+def map_to_square(source, array)
+
+end
